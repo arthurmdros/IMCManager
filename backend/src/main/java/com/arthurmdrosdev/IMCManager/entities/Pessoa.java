c2fa3da6486f36b50b89fed0c8fcedc4dcdf38e7
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-14 22:11:26 -0300

    Adding atribute id because i forgot

--- backend/src/main/java/com/arthurmdrosdev/IMCManager/entities/Pessoa.java
+++ backend/src/main/java/com/arthurmdrosdev/IMCManager/entities/Pessoa.java
@@ -14,6 +14,7 @@ public class Pessoa {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private Date data_nasc;
 	private String cpf;
@@ -22,6 +23,14 @@ public class Pessoa {
 	private Double peso;
 	
 	public Pessoa() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
