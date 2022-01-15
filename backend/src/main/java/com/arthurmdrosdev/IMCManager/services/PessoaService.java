8d22100da66581dce871b12e7c7a23ae4190ce8f
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-15 10:34:54 -0300

    Updating and adding new methods of transactionals in data

--- backend/src/main/java/com/arthurmdrosdev/IMCManager/services/PessoaService.java
+++ backend/src/main/java/com/arthurmdrosdev/IMCManager/services/PessoaService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.arthurmdrosdev.IMCManager.dto.PessoaDTO;
 import com.arthurmdrosdev.IMCManager.entities.Pessoa;
@@ -34,5 +35,45 @@ public class PessoaService {
 		Pessoa result = repository.findById(id).get();
 		PessoaDTO dto = new PessoaDTO(result);
 		return dto;
+	}
+	
+	@Transactional
+	public PessoaDTO savePessoa(PessoaDTO dto) {		
+		Pessoa pessoa = new Pessoa();
+		
+		pessoa.setNome(dto.getNome());
+		pessoa.setData_nasc(dto.getData_nasc());
+		pessoa.setCpf(dto.getCpf());
+		pessoa.setSexo(dto.getSexo());
+		pessoa.setAltura(dto.getAltura());
+		pessoa.setPeso(dto.getPeso());
+		
+		pessoa = repository.saveAndFlush(pessoa);
+		
+		return new PessoaDTO(pessoa);
+	}
+	
+	@Transactional
+	public PessoaDTO updatePessoa(PessoaDTO dto, Long id) {	
+		Pessoa pessoaFound = repository.getById(id);
+		if(pessoaFound != null) {
+			pessoaFound.setNome(dto.getNome());
+			pessoaFound.setData_nasc(dto.getData_nasc());
+			pessoaFound.setCpf(dto.getCpf());
+			pessoaFound.setSexo(dto.getSexo());
+			pessoaFound.setAltura(dto.getAltura());
+			pessoaFound.setPeso(dto.getPeso());		
+		}		
+		pessoaFound = repository.saveAndFlush(pessoaFound);
+		
+		return new PessoaDTO(pessoaFound);
+	}
+	
+	@Transactional
+	public void deletePessoa(@PathVariable Long id) {
+		Pessoa pessoaFound = repository.findById(id).get();
+		if(pessoaFound != null) {
+			repository.delete(pessoaFound);
+		}		
 	}
 }
