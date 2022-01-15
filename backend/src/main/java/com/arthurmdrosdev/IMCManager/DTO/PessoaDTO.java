bf6aefc4fb1b6dcc677981821e8428d6a32e59a0
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-15 13:01:36 -0300

    Adding classIMC atribute in constructor of PessoaDTO

--- backend/src/main/java/com/arthurmdrosdev/IMCManager/DTO/PessoaDTO.java
+++ backend/src/main/java/com/arthurmdrosdev/IMCManager/DTO/PessoaDTO.java
@@ -2,6 +2,7 @@ package com.arthurmdrosdev.IMCManager.dto;
 
 import java.sql.Date;
 
+import com.arthurmdrosdev.IMCManager.entities.ClassificationIMC;
 import com.arthurmdrosdev.IMCManager.entities.Pessoa;
 
 public class PessoaDTO {
@@ -13,10 +14,11 @@ public class PessoaDTO {
 	private String sexo;
 	private Double altura;
 	private Double peso;
+	private ClassificationIMC classIMC;
 	
 	public PessoaDTO() {}
 
-	public PessoaDTO(Long id, String pNome, Date pData, String pCpf, String pSexo, Double pAltura, Double pPeso) {
+	public PessoaDTO(Long id, String pNome, Date pData, String pCpf, String pSexo, Double pAltura, Double pPeso, ClassificationIMC pClassIMC) {
 		super();
 		this.id = id;
 		this.nome = pNome;
@@ -25,6 +27,7 @@ public class PessoaDTO {
 		this.sexo = pSexo;
 		this.altura = pAltura;
 		this.peso = pPeso;
+		this.classIMC = pClassIMC;
 	}
 
 	public Long getId() {
@@ -43,6 +46,7 @@ public class PessoaDTO {
 		sexo = pessoa.getSexo();
 		altura = pessoa.getAltura();
 		peso = pessoa.getPeso();
+		classIMC = pessoa.getClassIMC();
 	}
 
 	public String getNome() {
@@ -93,5 +97,12 @@ public class PessoaDTO {
 		this.peso = peso;
 	}
 	
+	public ClassificationIMC getClassIMC() {
+		return classIMC;
+	}
+
+	public void setClassIMC(ClassificationIMC classIMC_id) {
+		this.classIMC = classIMC_id;
+	}
 	
 }
