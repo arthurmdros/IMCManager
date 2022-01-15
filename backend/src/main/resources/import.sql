70842b407910bb49f9976118c6d4376e10d329e0
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-15 13:01:59 -0300

    Updating import.sql with the new inserts into table tb_classification

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,5 +1,12 @@
-INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('joao','1998-05-26','111.222.333-44','M',1.81,65.0);
-INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('maria','1995-03-01','555.666.777-88','F',1.65,56.8);
-INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('rafael','1999-10-30','999.000.111-22','M',1.76,83.0);
-INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('laura','2000-02-11','333.444.555-66','F',1.70,58.0);
-INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('ingrid','1990-08-25','777.888.999-00','F',1.60,60.5);
+INSERT INTO tb_classification(nome) VALUES ('Magreza');
+INSERT INTO tb_classification(nome) VALUES ('Normal');
+INSERT INTO tb_classification(nome) VALUES ('Sobrepeso');
+INSERT INTO tb_classification(nome) VALUES ('Obesidade grau I');
+INSERT INTO tb_classification(nome) VALUES ('Obesidade grau II');
+INSERT INTO tb_classification(nome) VALUES ('Obesidade grau III');
+
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso, classIMC_id) VALUES ('joao','1998-05-26','111.222.333-44','M',1.81,98.7,4);
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso, classIMC_id) VALUES ('maria','1995-03-01','555.666.777-88','F',1.65,56.8,2);
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso, classIMC_id) VALUES ('rafael','1999-10-30','999.000.111-22','M',1.76,83.0,3);
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso, classIMC_id) VALUES ('laura','2000-02-11','333.444.555-66','F',1.70,58.0,2);
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso, classIMC_id) VALUES ('ingrid','1990-08-25','777.888.999-00','F',1.60,60.5,2);
