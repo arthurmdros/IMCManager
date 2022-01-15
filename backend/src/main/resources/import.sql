80d1361afd0893878c9704ffe5128c4dabd82099
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-14 22:12:14 -0300

    Adding scripts sql to insert into table pessoa

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('joao','1998-05-26','111.222.333-44','M',1.81,65.0);
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('maria','1995-03-01','555.666.777-88','F',1.65,56.8);
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('rafael','1999-10-30','999.000.111-22','M',1.76,83.0);
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('laura','2000-02-11','333.444.555-66','F',1.70,58.0);
+INSERT INTO tb_pessoa(nome, data_nasc, cpf, sexo, altura, peso) VALUES ('ingrid','1990-08-25','777.888.999-00','F',1.60,60.5);
