7c0ecffc4b8ee553b5d4045120233e027f4d3bed
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-14 21:04:43 -0300

    Adding entity Pessoa

---
+++ backend/src/main/java/com/arthurmdrosdev/IMCManager/entities/Pessoa.java
@@ -0,0 +1,73 @@
+package com.arthurmdrosdev.IMCManager.entities;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String nome;
+	private Date data_nasc;
+	private String cpf;
+	private String sexo;
+	private Double altura;
+	private Double peso;
+	
+	public Pessoa() {}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getData_nasc() {
+		return data_nasc;
+	}
+
+	public void setData_nasc(Date data_nasc) {
+		this.data_nasc = data_nasc;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public Double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Double altura) {
+		this.altura = altura;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}	
+}
