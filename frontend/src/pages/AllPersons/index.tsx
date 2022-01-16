8108249370d3e71172804ec2d7e0d4529af704d6
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-16 14:22:07 -0300

    Adding page to list all persons

---
+++ frontend/src/pages/AllPersons/index.tsx
@@ -0,0 +1,11 @@
+function AllPersons() {
+
+    return (
+
+        <div className="persons-container"> 
+            <h1>Pessoas cadastradas</h1>           
+        </div>
+    );
+}
+
+export default AllPersons;
