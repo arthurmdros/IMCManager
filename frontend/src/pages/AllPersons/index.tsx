33e1075a8b673380153e549c68c97e6142349d92
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-17 12:10:33 -0300

    Updating styles of page AllPersons and adding component PersonInfo

--- frontend/src/pages/AllPersons/index.tsx
+++ frontend/src/pages/AllPersons/index.tsx
@@ -1,11 +1,16 @@
-import Navbar from "components/NavBar";
+import PersonInfo from "components/PersonInfo";
+import "./styles.css";
 
 function AllPersons() {
 
     return (
-        <div className="persons-container"> 
-            <Navbar />
-            <h1>Pessoas cadastradas</h1>           
+        <div className="container">
+            <h1>Pessoas cadastradas</h1>
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                    <PersonInfo />
+                </div>
+            </div>
         </div>
     );
 }
