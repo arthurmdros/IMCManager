431de27376e30dc1749053a0b5258f77d1c4fa70
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-17 12:59:38 -0300

    Removing again component navbar in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from 'pages/Form';
 import AllPersons from "pages/AllPersons";
-import Navbar from "components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/pessoas" element={<AllPersons />} />
         <Route path="/pessoas/form" element={<Form />} />
