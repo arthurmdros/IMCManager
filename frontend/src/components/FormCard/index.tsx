436d4d75843d83e6124da104a1415c3fbc88f5da
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-17 13:01:21 -0300

    Adding component navbar, new inputs with types and function to get date

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,57 +1,78 @@
+import Navbar from 'components/NavBar';
 import './styles.css';
+
+function getDateAtual() {
+    var data = new Date();
+    var dia = String(data.getDate()).padStart(2, '0');
+    var mes = String(data.getMonth() + 1).padStart(2, '0');
+    var ano = data.getFullYear();
+    var dataAtual = ano + '-' + mes + '-' + dia;
+    return dataAtual.toString();
+
+}
 
 function FormCard() {
     return (
-        <div className="register-container">
-            <div className="content">
-                <section>
-                    <h1>Registro na plataforma</h1>
-                    <p>Faça seu cadastro, entre na plataforma e calcule informações referentes a sua saúde corporal.</p>                    
-                </section>
-                <form>
-                    <input
-                        placeholder="Nome"
-                        value="nome"
-                    />
-                    <input
-                        placeholder="CPF"
-                        value="cpf"
-                    />
-                    <div className="field-group">
+        <>
+            <Navbar link="/pessoas" label="Já possuo cadastro" />
+            <div className="register-container">
+                <div className="content">
+                    <section>
+                        <h1>Registro na plataforma</h1>
+                        <p>Faça seu cadastro, entre na plataforma e calcule informações referentes a sua saúde corporal.</p>
+                    </section>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Nome"
+                            id="nome"
+                        />
+                        <input
 
-                        <div className="field">
-                            <input
-                                placeholder="Data de nascimento"
-                                value="data_nasc"
-                            />
+                            type="text"
+                            placeholder="CPF"
+                            id="cpf"
+                        />
+                        <div className="field-group">
+
+                            <div className="field">
+                                <input id="date" type="date"
+                                    value={getDateAtual()}
+                                />
+                            </div>
+                            <div className="field">
+                                <select className="select-input" id="sexo" placeholder='Sexo'>
+                                    <option value="" selected disabled hidden>Sexo</option>
+                                    <option>Masculino</option>
+                                    <option>Feminino</option>
+                                    <option>Prefiro não dizer</option>
+                                    <option>Outro</option>
+                                </select>
+                            </div>
                         </div>
-                        <div className="field">
-                            <select className="select-input" id="sexo" placeholder='Sexo'>
-                                <option>Masculino</option>
-                                <option>Feminino</option>
-                                <option>Prefiro não dizer</option>
-                                <option>Outro</option>
-                            </select>
+                        <div className="field-group">
+                            <div className="field">
+                                <input
+                                    type="number"
+                                    step="0.01" min="0"
+                                    placeholder="Altura"
+                                    id="altura"
+                                />
+                            </div>
+                            <div className="field">
+                                <input
+                                    type="number"
+                                    step="0.01" min="0"
+                                    placeholder="Peso"
+                                    id="peso"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <div className="field-group">
-                        <div className="field">
-                            <input
-                                placeholder="Altura"
-                                value="altura"
-                            />
-                        </div>
-                        <div className="field">
-                            <input
-                                placeholder="Peso"
-                                value="peso"
-                            />
-                        </div>
-                    </div>
-                    <button className="button" type="submit">Cadastrar</button>
-                </form>
+                        <button className="button" type="submit">Cadastrar</button>
+                    </form>
+                </div >
             </div >
-        </div >
+        </>
     );
 }
 
