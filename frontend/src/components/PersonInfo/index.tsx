51e403917303d6d3e6edaa061154387683b6110c
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-17 13:04:06 -0300

    Updating css and elements of PersonInfo component

--- frontend/src/components/PersonInfo/index.tsx
+++ frontend/src/components/PersonInfo/index.tsx
@@ -4,22 +4,27 @@ function PersonInfo() {
     return (
         <div className='personInfo'>
             <div key="id">
+                
+            <div className="info-group">
                 <h3>Nome:</h3>
-                <p>"Fulano de tal"</p>
-
-                <h3>Peso:</h3>
-                <p>"86 kg"</p>
-
-                <h3>Altura:</h3>
-                <p>"1,85"</p>
-
-                <button className="btn-peso" type="button">
-                    Peso ideal
-                </button>
-
-                <button className="btn-imc" type="button">
-                    IMC
-                </button>
+                <p>Fulano de tal</p>
+                </div>
+                <div className="weight-group">
+                    <h3>Peso:</h3>
+                    <p>86 kg</p>
+                </div>
+                <div className="height-group">
+                    <h3>Altura:</h3>
+                    <p>1,85</p>
+                </div>
+                <div className="btn-group">
+                    <button type="button">
+                        Peso ideal
+                    </button>
+                    <button type="button">
+                        IMC
+                    </button>
+                </div>
             </div>
         </div>
     );
