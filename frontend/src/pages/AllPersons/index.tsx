6ecdf5ee95d0451387602aff23dc0862680c706e
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-17 13:02:10 -0300

    Adding navbar component with news updates

--- frontend/src/pages/AllPersons/index.tsx
+++ frontend/src/pages/AllPersons/index.tsx
@@ -1,17 +1,21 @@
+import Navbar from "components/NavBar";
 import PersonInfo from "components/PersonInfo";
 import "./styles.css";
 
 function AllPersons() {
 
     return (
-        <div className="container">
-            <h1>Pessoas cadastradas</h1>
-            <div className="row">
-                <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
-                    <PersonInfo />
+        <>
+            <Navbar link="/pessoas/form" label="Faça seu cadastro" />
+            <div className="container">
+                <h1>Pessoas cadastradas</h1>
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3" >
+                        <PersonInfo />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
