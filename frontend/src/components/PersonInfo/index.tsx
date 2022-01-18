14c5e2f6720ccd66dbd3a4f5b95f27776ae4158a
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-18 20:17:02 -0300

    Adding people object properties to display values ​​obtained from axios get

--- frontend/src/components/PersonInfo/index.tsx
+++ frontend/src/components/PersonInfo/index.tsx
@@ -1,21 +1,37 @@
+import { Persons } from 'types/Persons';
+import { DateConvert } from 'utils/convertDate';
 import './styles.css';
 
-function PersonInfo() {
+type Props = {
+    person: Persons;
+}
+function PersonInfo({ person }: Props) {
+
     return (
         <div className='personInfo'>
-            <div key="id">
-                
-            <div className="info-group">
-                <h3>Nome:</h3>
-                <p>Fulano de tal</p>
+            <div>
+                <div className="info-group">
+                    <h3>Nome:</h3>
+                    <p>{person.nome}</p>
+                </div>
+                <div className="info-group">
+                    <h3>CPF:</h3>
+                    <p>{person.cpf}</p>
+                </div>
+                <div className="info-group">
+                    <h3>Data:</h3>
+                    <p>{DateConvert(person.data_nasc)}</p>
+                </div>
+                <div className="info-group">
+                    <h3>IMC:</h3>
+                    <p>{person.classIMC.nome}</p>
                 </div>
                 <div className="weight-group">
                     <h3>Peso:</h3>
-                    <p>86 kg</p>
-                </div>
-                <div className="height-group">
+                    <p>{person.peso}</p>
+
                     <h3>Altura:</h3>
-                    <p>1,85</p>
+                    <p>{person.altura}</p>
                 </div>
                 <div className="btn-group">
                     <button type="button">
