9c7e1fe7a8281cb3812d2c27288b3ca7e9214b57
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-18 20:09:11 -0300

    Adding type of Person entity

---
+++ frontend/src/types/Persons.ts
@@ -0,0 +1,25 @@
+export type classIMC = {
+    id: number;
+    nome: string;
+}
+export type Persons = {
+    id: number;
+    nome: string;
+    data_nasc: string;
+    cpf: string;
+    sexo: string;
+    altura: number;
+    peso: number;
+    classIMC: classIMC;
+}
+export type ContentPersons = {
+    content: Persons[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size: number;
+    number: number;
+    first: boolean;
+    numberOfElements: number;
+    empty: boolean;
+}
