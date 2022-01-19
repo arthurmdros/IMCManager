708388479e921441aba79272cf57114ff85be01d
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-18 21:30:37 -0300

    Adding export function DateFormat to save in API

--- frontend/src/utils/convertDate.ts
+++ frontend/src/utils/convertDate.ts
@@ -2,3 +2,8 @@ export function DateConvert(data: any){
     const [dd, mm, yyyy] = data.split('-');
     return `${yyyy}/${mm}/${dd}`;
 }
+
+export function DateConvertAnother(data: any){
+    const [dd, mm, yyyy] = data.split('/');
+    return `${yyyy}-${mm}-${dd}`;
+}
