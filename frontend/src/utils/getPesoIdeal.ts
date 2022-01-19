db26c7367b2df0a69feb44d701e2c6175ab11076
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-19 16:51:29 -0300

    Adding function to getPesoIdeal of Person

---
+++ frontend/src/utils/getPesoIdeal.ts
@@ -0,0 +1,11 @@
+export function getPesoIdeal(sexo: string, altura: number) {
+    if (sexo === "M") {
+        const result = (72.7 * altura) - 58;
+        return result.toFixed(2);
+    } else if (sexo === "F") {
+        const result = (62.1 * altura ) - 44.7;
+        return result.toFixed(2);        
+    } else {
+        return "impossível calcular";
+    }
+}
