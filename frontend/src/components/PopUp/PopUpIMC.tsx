fbf0b963de786398b3a0501c13c739d092ad1feb
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-20 21:10:56 -0300

    Adding classIMC update route with axios

--- frontend/src/components/PopUp/PopUpIMC.tsx
+++ frontend/src/components/PopUp/PopUpIMC.tsx
@@ -1,21 +1,61 @@
+import axios, { AxiosRequestConfig } from "axios";
 import React from "react";
+import { Persons } from "types/Persons";
+import { getIMC } from "utils/getIMC";
+import { BASE_URL } from "utils/requests";
 import { DesktopPopUpIMCContainer, Header } from "./PopUpIMC.styles";
 import PopUpIMCWrapper from "./PopUpIMCWrapper";
 
 interface PopUpIMCWrapperProps {
     isPopUpIMCVisible: boolean;
     onBackdropClick: () => void;
-    message: string;
+    person: Persons;
 }
 
-const PopUpIMC: React.FC<PopUpIMCWrapperProps> = ({ onBackdropClick, isPopUpIMCVisible, message }) => {
+const PopUpIMC: React.FC<PopUpIMCWrapperProps> = ({ onBackdropClick, isPopUpIMCVisible, person }) => {  
     if (!isPopUpIMCVisible) { return null }
 
+    function handleClassIMC() {
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: `/pessoas/${person.id}/classIMC`,
+            data: {
+                classIMC: person.classIMC
+            }
+        }
+
+        axios(config);
+    }
+
+    const resultIMC = +getIMC(person.peso, person.altura);
+    var resultClassIMC = "";
+
+    if (resultIMC < 18.5) {
+        person.classIMC.id = 2;
+        resultClassIMC = "Magreza";
+    } else if (resultIMC > 18.5 && resultIMC < 24.9) {
+        person.classIMC.id = 3;
+        resultClassIMC = "Normal";
+    } else if (resultIMC > 24.9 && resultIMC < 29.9) {
+        person.classIMC.id = 4;
+        resultClassIMC = "Sobrepeso";
+    } else if (resultIMC > 29.9 && resultIMC < 34.9) {
+        person.classIMC.id = 5;
+        resultClassIMC = "Obesidade grau I";
+    } else if (resultIMC > 34.9 && resultIMC < 39.9) {
+        person.classIMC.id = 6;
+        resultClassIMC = "Obesidade grau II";
+    } else if (resultIMC > 39.9) {
+        person.classIMC.id = 7;
+        resultClassIMC = "Obesidade grau III";
+    }
+
     return (
-        <PopUpIMCWrapper onBackdropClick={onBackdropClick}>
+        <PopUpIMCWrapper onBackdropClick={() => {onBackdropClick(); handleClassIMC()}}>
             <DesktopPopUpIMCContainer>
                 <Header>
-                    O resultado é: {message}
+                    O resultado foi: <p>{resultIMC} - {resultClassIMC}</p>
                 </Header>
             </DesktopPopUpIMCContainer>
         </PopUpIMCWrapper>
