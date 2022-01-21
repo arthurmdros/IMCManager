db2e0cc30959ef543941f7d96b8eb69e0f208c88
arthurmdros <arthurmedeiros2005@gmail.com>
2022-01-20 21:11:39 -0300

    Updating styles to show text in popupIMC

--- frontend/src/components/PopUp/PopUpIMC.styles.ts
+++ frontend/src/components/PopUp/PopUpIMC.styles.ts
@@ -14,6 +14,7 @@ box-shadow: 0 0 32px rgba(0,0,0,0.5);
 padding: 40px;
 width: 450px;
 font-size: 26px;
+justify-content: center;
 `
 
 export const Header = styled.h3`
@@ -23,5 +24,4 @@ line-height: 1em;
 font-weight: 300;
 margin: 5px 0 10px;
 text-align: center;
-display: flex;
 `
